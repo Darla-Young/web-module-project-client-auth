aed8e25ec05be46fd4f324e1999b27af674920f4
Darla Young <89855627+Darla-Young@users.noreply.github.com>
2023-11-15 11:39:11 -0600

    header buttons functional with no auth

--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,17 @@
 import React from "react";
+import { useNavigate } from 'react-router-dom';
 
 export default function Header () {
+ const navigate = useNavigate();
 
  return (
   <div id="header" className="header container">
    <h2 className="header h2">FRIENDS DATABASE</h2>
    <div id="headbuttContainer" className="header button container">
-    <button className="header button login">LOGIN.</button>
-    <button className="header button friendlist">FRIENDLIST.</button>
-    <button className="header button addfriend">ADDFRIEND.</button>
-    <button className="header button logout">LOGOUT.</button>
+    <button className="header button login" onClick={()=>navigate('/login')} >LOGIN.</button>
+    <button className="header button friendlist" onClick={()=>navigate('/friendlist')} >FRIENDLIST.</button>
+    <button className="header button addfriend" onClick={()=>navigate('/addfriend')} >ADDFRIEND.</button>
+    <button className="header button logout" onClick={()=>navigate('/login')} >LOGOUT.</button>
    </div>
   </div>
  )
